1f76475c355c20b25470db4064584f34be2f550e
roaroaroa <pajuhy123@naver.com>
2017-07-03 14:13:19 +0900

    원 싸이클

--- askdjango/askdjango/settings.py
+++ askdjango/askdjango/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,8 +79,12 @@ WSGI_APPLICATION = 'askdjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+          'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'practicedb',
+        'USER': 'root',
+        'PASSWORD':'0000',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
